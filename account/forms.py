355d03ba7769bb04244c81d3c95c1e05730420b9
lgkiemde <74085491+lgkiemde@users.noreply.github.com>
2022-03-24 22:35:12 -0500

    Upload
    This has some errors. The landing page is actually /account/login/. There is an error with the {% form.as_p %} on the client_edit page.

---
+++ account/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import *
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegisterationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('phone', 'date_of_birth')
+
+
+class ClientForms(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ('first_name', 'last_name', 'date_of_birth', 'gender', 'address',
+                  'city', 'state', 'zip', 'email', 'phone', 'referred_by', 'reffered_to')
